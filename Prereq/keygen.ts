73181a7969981e3b8e99d017bba2ad7a997fa70a
Wayne <42037655+Monsterwayne@users.noreply.github.com>
2023-11-24 13:49:57 -0500

    Add files via upload

--- Prereq/keygen.ts
+++ Prereq/keygen.ts
@@ -2,8 +2,8 @@ import { Keypair } from "@solana/web3.js";
 
 //Generate a new keypair
 let kp = Keypair.generate()
-console.log(`You've generated a new Solana wallet: ${kp.publicKey.toBase58()}
 
-To save your wallet, copy and paste the following into a JSON file:
+console.log(`You've generated a new Solana wallet:
+${kp.publicKey.toBase58()}`)
 
-[${kp.secretKey}]`)
+console.log(`This is your private key!!! Do not share !![${kp.secretKey}]`)
